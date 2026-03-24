fa0bc58fae0756a833430205ce32b3f27d11cfe7
Ewa Jablonska <ewa.k.jablonska@gmail.com>
2026-03-24 21:51:08 +0100

    Revert "Multiple changes for the built"
    This reverts commit ecb8bae28a99c39e88b86564b1668bd2357430ed.

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://ewa6.github.io/portfolio-landing/',
+    base: '/portfolio-landing/',
+    trailingSlash: 'ignore',
+    vite: {
+      plugins: [tailwindcss()],
+    },
+
+  integrations: [react()]
+});
